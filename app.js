5ff4aba6eeaadda58b236a360ad04c561118a15e
Adrian Ramirez <prodash666@gmail.com>
2024-06-08 10:13:50 -0400

    actualizacion

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const bodyParser = require("body-parser");
 const { join } = require("path");
 const cookieParser = require("cookie-parser");
 const Database = require("./src/class/Database");
+const RouterHandler = require("./src/class/RouterHandler");
 
 const server = express();
 const database = new Database("./src/db/web_cooperativa.db");
+const routerHandler = new RouterHandler(server, database);
 
 server.set("PORT", 5433);
 
@@ -16,7 +18,8 @@ server.use( bodyParser.json() );
 server.use( bodyParser.urlencoded({ extended: false }) );
 server.use( cookieParser("test_123") );
 
-server.use( require("./src/routers/main") );
+/* server.use( require("./src/routers/main") ); */
+routerHandler.loadRouter("./src/routers/main");
 
 server.set("views", [ join(__dirname, "src", "views") ]);
 server.set("database", database);
@@ -31,3 +34,6 @@ server.engine("html", require("ejs").renderFile);
         console.log(`\n\n\n\n\n\nIngrese en el navegador la siguiente URL: "localhost:${server.get("PORT")}"`);
     });
 })();
+
+process.on("uncaughtException", console.error);
+process.on("unhandledRejection", console.error);
